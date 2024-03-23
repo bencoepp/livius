332c246a053939bc01bf7b77c4d1021f56f321b7
Ben Cöppicus <bencoepp@gmail.com>
2024-03-23 10:55:39 +0100

    Add several screens and update UI
    Created several screens for the app including home and settings screens. Also updated the UI which included adding widgets for deferred components. Integrated new package dependencies for better functionality and user experience. Created logic for checking the current environment being used. The windows code was refactored to support certain features such as hidden title bar, setting size and alignment.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,68 +1,384 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart' hide Page;
+import 'package:flutter/foundation.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:frontend/screens/home.dart';
+import 'package:frontend/screens/settings.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:system_theme/system_theme.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:window_manager/window_manager.dart';
+import 'theme.dart';
+import 'widgets/deferred_widget.dart';
 
-void main() {
-  runApp(const MyApp());
+const String appTitle = 'Livius';
+
+/// Checks if the current environment is a desktop environment.
+bool get isDesktop {
+  if (kIsWeb) return false;
+  return [
+    TargetPlatform.windows,
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+  ].contains(defaultTargetPlatform);
 }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // if it's not on the web, windows or android, load the accent color
+  if (!kIsWeb &&
+      [
+        TargetPlatform.windows,
+        TargetPlatform.android,
+      ].contains(defaultTargetPlatform)) {
+    SystemTheme.accentColor.load();
+  }
+
+  if (kIsWeb) {
+    setPathUrlStrategy();
+  }
+
+  if (isDesktop) {
+    await flutter_acrylic.Window.initialize();
+    if (defaultTargetPlatform == TargetPlatform.windows) {
+      await flutter_acrylic.Window.hideWindowControls();
+    }
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitleBarStyle(
+        TitleBarStyle.hidden,
+        windowButtonVisibility: false,
+      );
+      await windowManager.setMinimumSize(const Size(500, 640));
+      await windowManager.setSize(const Size(500, 640));
+      await windowManager.setAlignment(Alignment.bottomRight);
+      await windowManager.setAlwaysOnTop(false);
+      await windowManager.show();
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
+
+  runApp(
+    const MyApp(),
+  );
+
+  Future.wait([
+    //DeferredWidget.preload(general.loadLibrary),
+  ]);
+}
+
+final _appTheme = AppTheme();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ChangeNotifierProvider.value(
+      value: _appTheme,
+      builder: (context, child) {
+        final appTheme = context.watch<AppTheme>();
+        return FluentApp.router(
+          title: appTitle,
+          themeMode: appTheme.mode,
+          debugShowCheckedModeBanner: false,
+          color: appTheme.color,
+          darkTheme: FluentThemeData(
+            brightness: Brightness.dark,
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+            ),
+          ),
+          theme: FluentThemeData(
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+            ),
+          ),
+          locale: appTheme.locale,
+          builder: (context, child) {
+            return Directionality(
+              textDirection: appTheme.textDirection,
+              child: NavigationPaneTheme(
+                data: NavigationPaneThemeData(
+                  backgroundColor: appTheme.windowEffect !=
+                          flutter_acrylic.WindowEffect.disabled
+                      ? Colors.transparent
+                      : null,
+                ),
+                child: child!,
+              ),
+            );
+          },
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate,
+          routeInformationProvider: router.routeInformationProvider,
+        );
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({
+    super.key,
+    required this.child,
+    required this.shellContext,
+  });
+
+  final Widget child;
+  final BuildContext? shellContext;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  bool value = false;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // int index = 0;
+
+  final viewKey = GlobalKey(debugLabel: 'Navigation View Key');
+  final searchKey = GlobalKey(debugLabel: 'Search Bar Key');
+  final searchFocusNode = FocusNode();
+
+  late final List<NavigationPaneItem> originalItems = [
+    PaneItem(
+      key: const ValueKey('/'),
+      icon: const Icon(FluentIcons.home),
+      title: const Text('Home'),
+      body: const SizedBox.shrink(),
+    ),
+  ].map<NavigationPaneItem>((e) {
+    PaneItem buildPaneItem(PaneItem item) {
+      return PaneItem(
+        key: item.key,
+        icon: item.icon,
+        title: item.title,
+        body: item.body,
+        onTap: () {
+          final path = (item.key as ValueKey).value;
+          if (GoRouterState.of(context).uri.toString() != path) {
+            context.go(path);
+          }
+          item.onTap?.call();
+        },
+      );
+    }
+
+    if (e is PaneItemExpander) {
+      return PaneItemExpander(
+        key: e.key,
+        icon: e.icon,
+        title: e.title,
+        body: e.body,
+        items: e.items.map((item) {
+          if (item is PaneItem) return buildPaneItem(item);
+          return item;
+        }).toList(),
+      );
+    }
+    if (e is PaneItem) return buildPaneItem(e);
+    return e;
+  }).toList();
+
+  late final List<NavigationPaneItem> footerItems = [
+    PaneItemSeparator(),
+    PaneItem(
+      key: const ValueKey('/changelog'),
+      icon: const Icon(FluentIcons.change_entitlements),
+      title: const Text('Changelog'),
+      body: const SizedBox.shrink(),
+      onTap: () {
+        if (GoRouterState.of(context).uri.toString() != '/changelog') {
+          context.go('/changelog');
+        }
+      },
+    ),
+    PaneItem(
+      key: const ValueKey('/settings'),
+      icon: const Icon(FluentIcons.settings),
+      title: const Text('Settings'),
+      body: const SizedBox.shrink(),
+      onTap: () {
+        if (GoRouterState.of(context).uri.toString() != '/settings') {
+          context.go('/settings');
+        }
+      },
+    ),
+  ];
+
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
   }
 
   @override
+  void dispose() {
+    windowManager.removeListener(this);
+    searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  int topIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final localizations = FluentLocalizations.of(context);
+    final appTheme = context.watch<AppTheme>();
+    if (widget.shellContext != null) {
+      if (router.canPop() == false) {
+        setState(() {});
+      }
+    }
+
+    return NavigationView(
+      key: viewKey,
+      appBar: NavigationAppBar(
+        automaticallyImplyLeading: false,
+        leading: () {
+          final enabled = widget.shellContext != null && router.canPop();
+
+          final onPressed = enabled
+              ? () {
+                  if (router.canPop()) {
+                    context.pop();
+                    setState(() {});
+                  }
+                }
+              : null;
+          return NavigationPaneTheme(
+            data: NavigationPaneTheme.of(context).merge(NavigationPaneThemeData(
+              unselectedIconColor: ButtonState.resolveWith((states) {
+                if (states.isDisabled) {
+                  return ButtonThemeData.buttonColor(context, states);
+                }
+                return ButtonThemeData.uncheckedInputColor(
+                  FluentTheme.of(context),
+                  states,
+                ).basedOnLuminance();
+              }),
+            )),
+            child: Builder(
+              builder: (context) => PaneItem(
+                icon: const Center(child: Icon(FluentIcons.back, size: 12.0)),
+                title: Text(localizations.backButtonTooltip),
+                body: const SizedBox.shrink(),
+                enabled: enabled,
+              ).build(
+                context,
+                false,
+                onPressed,
+                displayMode: PaneDisplayMode.compact,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          );
+        }(),
+        title: () {
+          if (kIsWeb) {
+            return const Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(appTitle),
+            );
+          }
+          return const DragToMoveArea(
+            child: Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(appTitle),
             ),
-          ],
-        ),
+          );
+        }(),
+        actions: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Align(
+            alignment: AlignmentDirectional.centerEnd,
+            child: Padding(
+              padding: const EdgeInsetsDirectional.only(end: 8.0),
+              child: ToggleSwitch(
+                content: const Text('Dark Mode'),
+                checked: FluentTheme.of(context).brightness.isDark,
+                onChanged: (v) {
+                  if (v) {
+                    appTheme.mode = ThemeMode.dark;
+                  } else {
+                    appTheme.mode = ThemeMode.light;
+                  }
+                },
+              ),
+            ),
+          ),
+          if (!kIsWeb) const WindowButtons(),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      paneBodyBuilder: (item, child) {
+        final name =
+            item?.key is ValueKey ? (item!.key as ValueKey).value : null;
+        return FocusTraversalGroup(
+          key: ValueKey('body$name'),
+          child: widget.child,
+        );
+      },
+      pane: NavigationPane(
+        selected: topIndex,
+        onChanged: (index) => setState(() => topIndex = index),
+        displayMode: appTheme.displayMode,
+        indicator: () {
+          switch (appTheme.indicator) {
+            case NavigationIndicators.end:
+              return const EndNavigationIndicator();
+            case NavigationIndicators.sticky:
+            default:
+              return const StickyNavigationIndicator();
+          }
+        }(),
+        items: originalItems,
+        footerItems: footerItems,
+      ),
+      onOpenSearch: searchFocusNode.requestFocus,
+    );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FluentThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
       ),
     );
   }
 }
+
+final rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
+final router = GoRouter(navigatorKey: rootNavigatorKey, routes: [
+  ShellRoute(
+    navigatorKey: _shellNavigatorKey,
+    builder: (context, state, child) {
+      return MyHomePage(
+        shellContext: _shellNavigatorKey.currentContext,
+        child: child,
+      );
+    },
+    routes: [
+      GoRoute(
+          name: '/', path: '/', builder: (context, state) => const HomePage()),
+      GoRoute(
+          name: '/settings',
+          path: '/settings',
+          builder: (context, state) => const Settings()),
+    ],
+  ),
+]);
