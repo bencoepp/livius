46d86bfc9741426b2a2d56a52de172bb0f234926
Ben Cöppicus <bencoepp@gmail.com>
2024-03-27 20:38:59 +0100

    Add WeatherUtil class and enhance WeatherController class
    Added a new class WeatherUtil with methods to work with weather data, including pagination for retrieving list of weather. High-level explanations are provided for methods. Further enhanced the WeatherController class for ease of accessing weather data. WeatherRepository interface was updated to include a method for paginated weather data retrieval.

--- backend/libs/repositories/src/main/java/bencoepp/livius/repositories/weather/WeatherRepository.java
+++ backend/libs/repositories/src/main/java/bencoepp/livius/repositories/weather/WeatherRepository.java
@@ -1,11 +1,15 @@
 package bencoepp.livius.repositories.weather;
 
 import bencoepp.livius.entities.weather.Weather;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 
 /**
  * The WeatherRepository interface extends the MongoRepository interface.
  * It provides the CRUD operations for managing Weather objects in a MongoDB database.
  */
 public interface WeatherRepository extends MongoRepository<Weather, String> {
+    List<Weather> findByDateNotNull(Pageable pageable);
 }
---
+++ backend/services/weather/src/main/java/bencoepp/livius/weather/controller/WeatherController.java
@@ -0,0 +1,51 @@
+package bencoepp.livius.weather.controller;
+
+import bencoepp.livius.entities.weather.Weather;
+import bencoepp.livius.repositories.weather.WeatherRepository;
+import bencoepp.livius.utils.WeatherUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The WeatherController class is a REST controller that handles requests related to weather data.
+ */
+@RestController
+@RequestMapping("/api/v1/weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherRepository weatherRepository;
+    @Autowired
+    private WeatherUtil weatherUtil;
+    /**
+     * Retrieves all weather data.
+     *
+     * @param page the page number of the results to retrieve (default: 0)
+     * @param size the number of results per page (default: 24)
+     * @return a ResponseEntity containing a list of Weather objects
+     */
+    @GetMapping("/all")
+    public ResponseEntity<List<Weather>> allWeatherData(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "24") int size){
+        return ResponseEntity.ok(weatherUtil.getWeatherListFromPage(page, size));
+    }
+
+    /**
+     * Retrieves the weather information for a specific ID.
+     *
+     * @param id the ID of the weather information to retrieve
+     * @return a ResponseEntity containing the weather information if found, or a bad request response if not found
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Weather> getWeather(@PathVariable String id){
+        Optional<Weather> optionalWeather = weatherRepository.findById(id);
+        return optionalWeather.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    /*@GetMapping("/search")
+    public ResponseEntity<List<Weather>> searchWeather(){
+
+    }*/
+}
