f71b6ca86f33f4c7ba055efa51d4c09676aed3b5
Ben Cöppicus <bencoepp@gmail.com>
2024-03-31 22:21:51 +0200

    Implement AdaptiveScaffold and update UI in main.dart
    AdaptiveScaffold from the flutter_adaptive_scaffold package has been introduced to enhance the adaptive nature of the application. The HomePage widget has been significantly restructured with new NavigationDestination items and varying layout for different breakpoints. Furthermore, the app theme has been slightly adjusted, updating the title from 'Flutter Base Project' to 'Livius'.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Base Project',
+      title: 'Livius',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,21 +20,64 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
+  final _selectedTab = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton:
-          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to your Flutter base project!',
-          style: TextStyle(fontSize: 20.0),
+    // Define the children to display within the body at different breakpoints.
+    final List<Widget> children = <Widget>[
+      for (int i = 0; i < 10; i++)
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: Color.fromARGB(255, 122, 39, 33),
+            height: 400,
+          ),
+        )
+    ];
+
+    return AdaptiveScaffold(
+      transitionDuration: Duration(milliseconds: 300),
+      smallBreakpoint: const WidthPlatformBreakpoint(end: 700),
+      mediumBreakpoint: const WidthPlatformBreakpoint(begin: 700, end: 1000),
+      largeBreakpoint: const WidthPlatformBreakpoint(begin: 1000),
+      useDrawer: false,
+      selectedIndex: _selectedTab,
+      destinations: const <NavigationDestination>[
+        NavigationDestination(
+          icon: Icon(Icons.inbox_outlined),
+          selectedIcon: Icon(Icons.inbox),
+          label: 'Inbox',
         ),
+        NavigationDestination(
+          icon: Icon(Icons.article_outlined),
+          selectedIcon: Icon(Icons.article),
+          label: 'Articles',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.chat_outlined),
+          selectedIcon: Icon(Icons.chat),
+          label: 'Chat',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.video_call_outlined),
+          selectedIcon: Icon(Icons.video_call),
+          label: 'Video',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined),
+          selectedIcon: Icon(Icons.home),
+          label: 'Inbox',
+        ),
+      ],
+      body: (_) => GridView.count(crossAxisCount: 2, children: children),
+      smallBody: (_) => ListView.builder(
+        itemCount: children.length,
+        itemBuilder: (_, int idx) => children[idx],
       ),
+      secondaryBody: (_) => Container(
+        color: const Color.fromARGB(255, 234, 158, 192),
+      ),
+      smallSecondaryBody: AdaptiveScaffold.emptyBuilder,
     );
   }
 }
