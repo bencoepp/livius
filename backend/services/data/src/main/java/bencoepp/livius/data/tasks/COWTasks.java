6a4d3fdbdc3d75e9319ba611046e5931515305a4
Ben Cöppicus <bencoepp@gmail.com>
2024-04-02 17:44:19 +0200

    Add concurrent data importing functionality to Livius application
    Implemented functionality for concurrent downloading of data from correlatesofwar.org upon app launch. Created a new utility, COWUtil, which gathers all properties with a specific prefix. The COWTasks class has been updated with methods to initiate downloading of data and save them into desired files. This will allow for efficient data retrieval and storage as part of the application's startup operations. Additionally, the downloaded data directory is now included in the .gitignore file.

--- backend/services/data/src/main/java/bencoepp/livius/data/tasks/COWTasks.java
+++ backend/services/data/src/main/java/bencoepp/livius/data/tasks/COWTasks.java
@@ -1,9 +1,20 @@
 package bencoepp.livius.data.tasks;
 
+import bencoepp.livius.utils.COWUtil;
 import bencoepp.livius.utils.SequenceGeneratorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.CompletableFuture;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -12,4 +23,45 @@ public class COWTasks {
     @Autowired
     private SequenceGeneratorService sequenceGeneratorService;
 
+    @Autowired
+    private COWUtil util;
+
+    /**
+     * This method is triggered when the application is ready to start importing data.
+     * It downloads the source files from correlatesofwar.org concurrently and saves them with specified filenames.
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void startImport(){
+        log.info("Downloading sources from correlatesofwar.org");
+
+        Map<String, String> sources = util.getAllPropWithPrefix("livius.cow");
+        log.info("Gathered urls of cow sources");
+
+        // Use CompletableFuture to make downloadSource method be invoked concurrently.
+        sources.forEach((key, value) -> CompletableFuture.runAsync(() -> {
+            try {
+                String filename = value.substring(value.lastIndexOf("/") + 1);
+                downloadSource(value, key + filename.substring(filename.lastIndexOf(".")));
+            } catch (IOException e) {
+                log.error("Error occurred while downloading file from {}", value, e);
+            }
+        }));
+
+        log.info("Finished importing cow data");
+    }
+
+    /**
+     * Downloads the source file from the given URL and saves it with the specified filename.
+     *
+     * @param url      the URL of the source file to download
+     * @param filename the filename to save the downloaded file as
+     * @throws IOException if an I/O error occurs while downloading or saving the file
+     */
+    private void downloadSource(String url, String filename) throws IOException {
+        try (InputStream in = new URL(url).openStream()) {
+            Files.createDirectories(Path.of(System.getProperty("user.dir") + "/cow/downloaded"));
+            Files.copy(in, Path.of(System.getProperty("user.dir") + "/cow/downloaded/" + filename), StandardCopyOption.REPLACE_EXISTING);
+            log.info("Downloaded file from {}", url);
+        }
+    }
 }
