08374c8aa361ce71beb01b71e7a57aa32de77e26
Ben Cöppicus <bencoepp@gmail.com>
2024-04-03 16:52:44 +0200

    Add WarRegion and WarOutcome enums
    Created two new enum classes, WarRegion and WarOutcome, in the 'war' package. The WarRegion class represents possible war locations, while the WarOutcome class describes different possible outcomes of a war.

---
+++ backend/libs/entities/src/main/java/bencoepp/livius/entities/war/WarOutcome.java
@@ -0,0 +1,11 @@
+package bencoepp.livius.entities.war;
+
+public enum WarOutcome {
+    SIDE_A_WIN,
+    SIDE_B_WIN,
+    COMPROMISE,
+    TRANSFORM_INTO_WAR,
+    CONTINUING,
+    STALEMATE,
+    CONTINUE_BELOW_WAR_LEVEL
+}
