89dce6c0cab31f114b89ffddb4249fb2a59c2a67
Ben Cöppicus <bencoepp@gmail.com>
2024-04-10 16:08:51 +0200

    Add TerritorialChangeRepository interface
    A new interface, TerritorialChangeRepository, has been added to the state package. The version number of the artifact 'repositories' in the pom.xml file has been updated to reflect these changes. The new version also updates the version of the dependency 'entities'.

---
+++ backend/libs/repositories/src/main/java/bencoepp/livius/repositories/state/TerritorialChangeRepository.java
@@ -0,0 +1,7 @@
+package bencoepp.livius.repositories.state;
+
+import bencoepp.livius.entities.state.TerritorialChange;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TerritorialChangeRepository extends CrudRepository<TerritorialChange, String> {
+}
