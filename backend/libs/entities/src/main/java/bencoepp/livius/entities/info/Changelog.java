825b9e38c6e8cf6cd067a5737643a2e862287021
Ben Cöppicus <bencoepp@gmail.com>
2024-04-15 20:54:56 +0200

    Remove 'Role' and 'User' models and refactor 'UserCard' widget
    The 'Role' and 'User' model files from the frontend library have been deleted. On top of this, the 'UserCard' widget has been refactored to use FutureBuilder and display dynamic user data. Additionally, necessary dependencies such as 'http' and 'shared_preferences' have been added for managing HTTP requests and local storage.

--- backend/libs/entities/src/main/java/bencoepp/livius/entities/info/Changelog.java
+++ backend/libs/entities/src/main/java/bencoepp/livius/entities/info/Changelog.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
-import javax.xml.crypto.dsig.keyinfo.KeyValue;
 import java.util.ArrayList;
 import java.util.List;
 
--- frontend/lib/widgets/user/user_card.dart
+++ frontend/lib/widgets/user/user_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/model/user/user.dart';
 
 class UserCard extends StatefulWidget {
   const UserCard({super.key});
@@ -10,18 +11,34 @@ class UserCard extends StatefulWidget {
 class _UserCardState extends State<UserCard> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Column(
-          children: [
-            ListTile(
-              title: Text('USERNAME'),
-              subtitle: Text('ID'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: User.getMe(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.done) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(snapshot.data!.username),
+                      subtitle: Text(snapshot.data!.id),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          }
+        });
   }
 }
